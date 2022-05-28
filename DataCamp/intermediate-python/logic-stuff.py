1d2a169398fba2dc1e03f2ff190ab9a5a2b41497
muhammadhaykal02 <70922841+muhammadhaykal02@users.noreply.github.com>
2022-05-28 15:14:16 +0700

    logical

---
+++ DataCamp/intermediate-python/logic-stuff.py
@@ -0,0 +1,33 @@
+#DONT FORGET THE : to make an indent
+# Define variables
+room = "kit"
+area = 14.0
+
+# if statement for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+
+# if statement for area
+if area > 15.0 :
+    print("big place!")
+
+================================
+# Define variables
+room = "bed"
+area = 14.0
+
+# if-elif-else construct for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+elif room == "bed":
+    print("looking around in the bedroom.")
+else :
+    print("looking around elsewhere.")
+
+# if-elif-else construct for area
+if area > 15 :
+    print("big place!")
+elif area > 10 :
+    print("medium size, nice!")
+else :
+    print("pretty small.")
