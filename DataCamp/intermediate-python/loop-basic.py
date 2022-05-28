944adb86eec873e16449460674a5492b9feb6f92
muhammadhaykal02 <70922841+muhammadhaykal02@users.noreply.github.com>
2022-05-28 21:58:32 +0700

    everything about LOOPS

--- DataCamp/intermediate-python/loop-basic.py
+++ DataCamp/intermediate-python/loop-basic.py
@@ -55,3 +55,73 @@ house = [["hallway", 11.25],
 # Build a for loop from scratch
 for x in house :
     print("the " + x[0] + " is " + str(x[1]) + " sqm")
+#the list isn't paired, so you can always use x[0] and x[1] to refer the list.
+
+==============================
+#loop over dictionary
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw', 'austria':'vienna' }
+          
+# Iterate over europe
+for country, capital in europe.items() :
+    print("the capital of " + country + " is " + capital)
+    
+====================================
+#loop over numpy array
+
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height, a 1D numpy
+for height in np_height :
+    print(str(height) + " inches")
+
+# For loop over np_baseball, a 2D numpy
+for data in np.nditer(np_baseball) :
+    print(data)
+
+==================================
+#loop over data frame (panda series)
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Iterate over rows of cars
+for lab, row in cars.iterrows() :
+    print(lab)
+    print(row)
+    
+=========================
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Adapt for loop, this is an item (string) so you need to use [0] instead of the name of the column/row.
+for lab, row in cars.iterrows() :
+    print(lab + ": " + str(row[0]))
+    
+=========================================
+#how to add a new column in your dataframe, adding country name in uppercase (.upper() method)
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Code for loop that adds COUNTRY column
+for lab, row in cars.iterrows() :
+    cars.loc[lab, "COUNTRY"] = row["country"].upper()
+
+# Print cars
+print(cars)
+==================================
+#adding new column to dataframe in oneliner
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Use .apply(str.upper), the left refers to the column name we want and the right is the column we are referring to.
+cars["COUNTRY"] = cars["country"].apply(str.upper)
+print(cars)
