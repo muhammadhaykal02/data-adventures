ef2734b9570dd8f826daa1041acc2b20b7853d7e
muhammadhaykal02 <70922841+muhammadhaykal02@users.noreply.github.com>
2022-05-31 23:41:36 +0700

    aggregating data

---
+++ DataCamp/data-manipulation/data-aggregation.py
@@ -0,0 +1,44 @@
+# Print the head of the sales DataFrame
+print(sales.head())
+
+# Print the info about the sales DataFrame
+print(sales.info())
+
+# Print the mean of weekly_sales
+print(sales["weekly_sales"].mean())
+
+# Print the median of weekly_sales
+print(sales["weekly_sales"].median())
+
+# Print the maximum of the date column
+print(sales["date"].max())
+
+# Print the minimum of the date column
+print(sales["date"].min())
+
+## Using .agg() ##
+
+# A custom IQR function
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+    
+# Print IQR of the temperature_c column
+print(sales["temperature_c"].agg(iqr))
+
+### for multiple columns ###
+
+# A custom IQR function
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+
+# Update to print IQR of temperature_c, fuel_price_usd_per_l, & unemployment
+print(sales[["temperature_c", "fuel_price_usd_per_l", "unemployment"]].agg(iqr))
+
+### multiple .agg() ###
+# Import NumPy and create custom IQR function
+import numpy as np
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+
+# Update to print IQR and median of temperature_c, fuel_price_usd_per_l, & unemployment
+print(sales[["temperature_c", "fuel_price_usd_per_l", "unemployment"]].agg([iqr, np.median]))
