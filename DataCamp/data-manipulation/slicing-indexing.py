a45e49b2a60157c2f06bafc0daece6a77f12e5f0
muhammadhaykal02 <70922841+muhammadhaykal02@users.noreply.github.com>
2022-06-02 14:03:35 +0700

    Update slicing-indexing.py

--- DataCamp/data-manipulation/slicing-indexing.py
+++ DataCamp/data-manipulation/slicing-indexing.py
@@ -100,3 +100,26 @@ print(temperatures.iloc[:, 2:4])
 
 # Use slicing in both directions at once
 print(temperatures.iloc[:5,2:4])
+
+======================
+## creating a pivot table ##
+# Add a year column to temperatures
+temperatures["year"] = temperatures["date"].dt.year
+
+# Pivot avg_temp_c by country and city vs year
+temp_by_country_city_vs_year = temperatures.pivot_table("avg_temp_c", index=["country","city"], columns="year")
+
+# See the result
+print(temp_by_country_city_vs_year)
+
+=======================
+## subsetting pivot table ##
+# Subset for Egypt to India
+temp_by_country_city_vs_year.loc["Egypt": "India"]
+
+# Subset for Egypt, Cairo to India, Delhi
+temp_by_country_city_vs_year.loc[("Egypt", "Cairo"): ("India", "Delhi")]
+
+# Subset for Egypt, Cairo to India, Delhi, and 2005 to 2010
+temp_by_country_city_vs_year.loc[("Egypt", "Cairo"): ("India", "Delhi"), "2005":"2010"]
+
