a4c393048d5c4c3cfbbe84f80f53d6aed43cf516
muhammadhaykal02 <70922841+muhammadhaykal02@users.noreply.github.com>
2022-06-02 14:15:39 +0700

    Update slicing-indexing.py

--- DataCamp/data-manipulation/slicing-indexing.py
+++ DataCamp/data-manipulation/slicing-indexing.py
@@ -123,3 +123,16 @@ temp_by_country_city_vs_year.loc[("Egypt", "Cairo"): ("India", "Delhi")]
 # Subset for Egypt, Cairo to India, Delhi, and 2005 to 2010
 temp_by_country_city_vs_year.loc[("Egypt", "Cairo"): ("India", "Delhi"), "2005":"2010"]
 
+==========================
+## calculating pivot table ##
+# Get the worldwide mean temp by year
+mean_temp_by_year = temp_by_country_city_vs_year.mean()
+
+# Filter for the year that had the highest mean temp
+print(mean_temp_by_year[mean_temp_by_year == mean_temp_by_year.max()])
+
+# Get the mean temp by city
+mean_temp_by_city = temp_by_country_city_vs_year.mean(axis="columns")
+
+# Filter for the city that had the lowest mean temp
+print(mean_temp_by_city[mean_temp_by_city == mean_temp_by_city.min()])
